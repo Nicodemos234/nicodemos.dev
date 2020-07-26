73186ae5bc72e4efbbc63aee056a92daf8fa1ed1
Nicodemos234 <nicodemos54324@gmail.com>
2020-07-25 23:21:46 -0300

    Contact start

---
+++ components/Form.js
@@ -0,0 +1,11 @@
+import styles from './Form.module.css'
+
+function Form() {
+    return <div>
+        <form>
+            <input></input>
+        </form>
+    </div>
+}
+
+export default Form
--- pages/contact.js
+++ pages/contact.js
@@ -2,15 +2,18 @@ import dynamic from "next/dynamic"
 import Head from 'next/head'
 
 const Header = dynamic(import("../components/Header"));
+const Form = dynamic(import('../components/Form'));
 
 function Contact() {
-    return <div>
+    return <div className="container-main">
         <Head>
         <title>Contact - Nicodemos.dev</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
         <Header />
-        works
+        <div className="nico-flex" >
+            <Form />
+        </div>
     </div>
 }
 
