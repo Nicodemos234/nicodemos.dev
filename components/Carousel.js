b179fdd56f251811a85cff7e78f89a4d6ebc802c
Nicodemos234 <nicodemos54324@gmail.com>
2020-07-26 12:56:43 -0300

    Works carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,27 +1,59 @@
 import styles from './Carousel.module.css'
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
 
-function CarouselW() {
-    return <div className={styles.carousel}>
-        <div className={styles.left}></div>
-        <div className={styles.content}>
+
+class CarouselC extends React.Component {
+
+    render() {
+        return <Carousel>
+            {/* <div className={styles.left}></div> */}
             <div className={styles.item}>
-                <div><a target="_blank" href="https://www.pechinchou.com.br"><img src="/assets/images/carousel/pechincha.png" /></a></div>
+                <div>
+                    <a target="_blank" href="https://www.pechinchou.com.br"><img src="/assets/images/carousel/pechincha.png" /></a>
+                </div>
                 <div>
                     Website developed using Wordpress
+                    </div>
+                <div>
+                    <a target="_blank" href="https://www.pechinchou.com.br">Pechinchou Site</a>
                 </div>
-                <div><a target="_blank" href="https://www.pechinchou.com.br">Pechinchou Site</a></div>
             </div>
-            {/* <div className={styles.item}>
-                <div><a target="_blank" href="https://www.pechinchou.com.br"><img src="/assets/images/carousel/pechincha.png" /></a></div>
+            <div className={styles.item}>
                 <div>
-                    Website developed using Wordpress
+                    <a target="_blank" href="https://www.siasp.com.br/"><img src="/assets/images/carousel/siasp.png" /></a>
                 </div>
-                <div><a target="_blank" href="https://www.pechinchou.com.br">Pechinchou Site</a></div>
-            </div> */}
-        </div>
-        <div className={styles.right}></div>
-    </div>
-
+                <div>
+                    Internship at Siasp
+                    </div>
+                <div>
+                    <a target="_blank" href="https://www.siasp.com.br/">SIASP</a>
+                </div>
+            </div>
+            <div className={styles.item}>
+                <div>
+                    <a target="_blank" href="https://www.pechinchou.com.br"><img src="/assets/images/carousel/pechinchaapp.png" /></a></div>
+                <div>
+                    App developed using Flutter
+                    </div>
+                <div>
+                    <a target="_blank" href="https://www.pechinchou.com.br">Pechinchou App (coming soon)</a>
+                </div>
+            </div>
+            <div className={styles.item}>
+                <div>
+                    <a target="_blank" href="https://www.quadrocriativo.com.br"><img src="/assets/images/carousel/quadro.png" /></a>
+                </div>
+                <div>
+                    Website developed using Next.js
+                    </div>
+                <div>
+                    <a target="_blank" href="https://www.quadrocriativo.com.br">Quadro Criativo Site (coming soon)</a>
+                </div>
+            </div>
+            {/* <div className={styles.right} onClick={this.nextPage}></div> */}
+        </Carousel>
+    }
 }
 
-export default CarouselW
+export default CarouselC
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../assets/styles/reset.css'
 import '../assets/styles/styles.css'
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
