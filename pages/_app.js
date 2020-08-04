11f19e695d10bde10b027bd3a11d11ca40a0ad67
Nicodemos234 <nicodemos54324@gmail.com>
2020-08-04 13:20:12 -0300

    Progress bar on top

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,32 @@
 import '../assets/styles/reset.css'
 import '../assets/styles/styles.css'
+import 'nprogress/nprogress.css'
+import Router from 'next/router'
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { PageTransition } from 'next-page-transitions'
 import dynamic from "next/dynamic"
 const Header = dynamic(import("../components/Header"))
+import NProgress from 'nprogress';
 
+
+Router.onRouteChangeStart = () => {
+  // console.log('onRouteChnageStart triggered');
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  // console.log('onRouteChnageComplete triggered');
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  // console.log('onRouteChnageError triggered');
+  NProgress.done();
+};
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps, router }) {
+
   return <>
     <Header />
     <PageTransition timeout={300} classNames="page-transition">
